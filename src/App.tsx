bf3645570a1252b9b68c247a67a4188eee641715
Grzdaczek <grzdaczek2727@gmail.com>
2020-04-09 17:32:19 +0200

    File loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react'
 import Boo from './components/Boo'
 import Foo from './components/Foo'
 import './styles/App.sass'
+import img from 'src/assets/images/img.jpg'
 
 const App: React.FunctionComponent = () =>  {
 	return (
 		<Fragment>
 			<Boo />
 			<Foo />
+			<img src={img} />
 		</Fragment>
 	)
 }
--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,47 @@
+const path = require('path')
+
 module.exports = {
 	entry: './src/index.tsx',
 	target: 'electron-renderer',
 	resolve: {
-		extensions: ['.ts', '.tsx', '.js']
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+		alias: {
+			src: path.resolve(__dirname, 'src/')
+		}
 	},
 	module: {
 		rules: [
 			{
+				test: /\.(jpe?g|png|gif|svg)$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'file-loader',
+					options: { name: 'assets/[name].[hash:8].[ext]' }
+				}
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2)$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'file-loader',
+					options: { name: 'assets/[name].[hash:8].[ext]' }
+				}
+			},
+			{
+				test: /\.(scss|sass)$/,
+				exclude: /(node_modules)/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			},
+			{
 				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
 				use: ['babel-loader']
 			},
 			{
 				test: /\.(ts|tsx)?$/,
-				use: ['ts-loader']
+				exclude: /(node_modules)/,
+				use: ['babel-loader', 'ts-loader']
 			},
-			{
-				test: /\.(scss|sass)$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-			// {
-			// 	test: /\.(jpe?g|png|gif)$/, // loader for images
-			// 	use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
-			// 	include: defaultInclude
-			// },
-			// {
-			// 	test: /\.(eot|svg|ttf|woff|woff2)$/, // loader for custom fonts
-			// 	use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-			// 	include: defaultInclude
-			// }
 		]
 	},
 }
